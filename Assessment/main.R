a90221cdd32cfb72154a81eff59555b2f2d341a6
Uju-Chinedum <ujuchimaraoke@gmail.com>
2022-06-09 09:00:18 +0100

    Updated assessment

--- Assessment/main.R
+++ Assessment/main.R
@@ -1,8 +1,159 @@
-                            ############
-                            #### Ex. 4.5
-                            ############
-                               # 1 #
-                               #####
+                                  #################
+                                  #### Ex. 4.5 ####
+                                  #################
+                                        # 1 #
+                                        #####
+
+sample(3:6, 50, replace = TRUE)
+
+                                        # 2 #
+                                        #####
+                          
+x <- sample(1:50, 1)
+y <- sample(2:5, 1)
+x ** y
+
+                                        # 3 #
+                                        #####
+
+step <- sample(1:10, 1)
+for (i in 1:step){
+  print("Chinedum")
+}
+
+                                        # 4 #
+                                        #####
+
+observation <- runif(1, 1, 10)
+round(observation, 2)
+
+                                        # 5 #
+                                        #####
+
+for (i in 1:50){
+  print(sample(1:i+1, 1))
+}
+
+                                        # 6 #
+                                        #####
+
+x <- as.numeric(readline("Enter a number: "))
+y <- as.numeric(readline("Enter a number: "))
+
+num <- x - y
+if (num < 0){
+  num <- num * -1
+}
+
+denom <- x + y
+ans <- num / denom
+print(ans)
+
+                                        # 7 #
+                                        #####
+
+
+
+
+
+
+
+
+
+                                        # 8 #
+                                        #####
+
+time <- as.integer(readline("Enter a time in seconds: "))
+
+minutes <- floor(time / 60)
+seconds <- time %% 60
+print(paste(time, "seconds is", minutes, "minutes", seconds, "seconds"))
+
+                                        # 9 #
+                                        #####
+
+time <- as.integer(readline("Enter a time in hours: "))
+future <- as.integer(readline("How many hours into the future do you wanna go?: "))
+
+new_time <- time + future
+if (new_time > 12){
+  new_time <- new_time %% 12
+}
+
+print(paste(new_time, "o'clock"))
+
+                                        # 10 #
+                                        ######
+# a #
+#####
+
+power <- as.numeric(readline("Enter a power: "))
+eval <- 2 ** power
+print(eval %% 10)
+
+# b #
+#####
+
+power <- as.numeric(readline("Enter a power: "))
+eval <- 2 ** power
+print(eval %% 100)
+
+# c #
+#####
+
+power <- as.numeric(readline("Enter a power: "))
+digits <- as.integer(readline("Enter how many digits you want: "))
+
+eval <- 2 ** power
+reaminder <- 10 ** digits
+print(eval %% remainder)
+
+                                    # 11 #
+                                    ######
+
+weight <- as.numeric(readline("Enter a weight in kg: "))
+pounds = weight * 2.205
+print(paste, (round(pounds, 1)), pounds )
+
+                                    # 12 #
+                                    ######
+
+number <- as.integer(readline("Enter a number: "))
+fact <- 1
+
+for (i in 1:number){
+  fact <- fact * i
+}
+
+print(fact)
+
+                                    # 13 #
+                                    ######
+
+number <- as.numeric(readline("Enter a number: "))
+
+sine <- sin(number)
+cosine <- cos(number)
+tangent <- tan(number)
+
+values <- c(sine, cosine, tangent)
+func <- c("Sine", "Cosine", "Tangent")
+names(values) <- func
+
+print(values)
+
+                                    # 14 #
+                                    ######
+
+number <- as.numeric(readline("Enter a number: "))
+print(sin(number))
+
+
+                            #################
+                            #### Ex. 4.5 ####
+                            #################
+                                   # 1 #
+                                   #####
 
 # taking input from user
 length <- as.numeric(readline("Enter length in cm: "))
@@ -15,8 +166,8 @@ if (length < 0){
     print(paste(length, "cm is", inch, "inches long.", sep = " "))
 }
 
-                               # 2 #
-                               #####
+                                   # 2 #
+                                   #####
 
 # taking input from user
 temp <- as.numeric(readline("Enter temperature: "))
@@ -32,8 +183,8 @@ if (unit == "f"){
     print(paste("The temperature in Fahrenheit is", F, "degrees"))
 }
 
-                               # 3 #
-                               #####
+                                   # 3 #
+                                   #####
 
 temperature <- as.numeric(readline("Enter temperature in Celsius: "))
 
@@ -53,8 +204,8 @@ if (temperature > 100){
     print("Temperature invalid, below freezing point")
 }
 
-                               # 4 #
-                               #####
+                                   # 4 #
+                                   #####
 
 credits <- as.integer(readline("How many credits have you taken? "))
 
@@ -69,8 +220,8 @@ if (credits >= 84){
 }
 
 
-                                # 5 #
-                                #####
+                                    # 5 #
+                                    #####
 
 number <- as.integer(readline("Enter a number: "))
 sum_div <- 0
@@ -83,10 +234,10 @@ for (i in 1:number){
 }
 print(sum_div)
 
-                               # 6 #
-                               #####
+                                 # 6 #
+                                 #####
 
-# finding all perfect squares from 1 to 10,000
+# finding all perfect numbers from 1 to 10,000
 for (number in 1:10000){
     div <- c()
     for (divisor in 1:number){
@@ -105,8 +256,8 @@ for (number in 1:10000){
     }
 } 
 
-                               # 7 #
-                               #####
+                                   # 7 #
+                                   #####
 
 number <- as.integer(readline("Enter number: "))
 div <- c()
@@ -132,8 +283,9 @@ if (length(div) == length(nsqn)){
     print("Not squarefree")
 }
 
-                               # 8 #
-                               #####
+                                   # 8 #
+                                   #####
+
 x <- as.numeric(readline("Enter a number: "))
 y <- as.numeric(readline("Enter a number: "))
 z <- as.numeric(readline("Enter a number: "))
@@ -149,10 +301,118 @@ print(x)
 print(y)
 print(z)
 
+                                   # 9 #
+                                   #####
 
 
+perfect_square <- 0
+perfect_cube <- 0
+perfect_fifth <- 0
+
+for (i in 1:1000){
+  square <- floor(i ** (1/2))
+  cube <- floor(i ** (1/3))
+  fifth <- floor(i ** (1/5))
+  
+  if ((square ** 2) != i){
+    perfect_square <- perfect_square + 1
+  }
+  
+  if ((cube ** 3) != i){
+    perfect_cube <- perfect_cube + 1
+  }
+  
+  if ((fifth ** 5) != i){
+    perfect_fifth <- perfect_fifth + 1
+  }
+}
+
+print(c(perfect_square, perfect_cube,perfect_fifth))
 
 
+                                    # 10 #
+                                    ######
 
+scores <- c()
+for (i in 1:10){
+  unit <- as.numeric(readline("Enter score: "))
+  scores <- c(scores, unit)
+}
 
+# a #
+#####
 
+# Getting the max and min scores
+maximum <- scores[1]
+minimum <- scores[1]
+
+for (score in scores){
+  if (score > maximum){
+    maximum <- score
+    
+  } else if (score < minimum) {
+    minimum <- score
+  }
+}
+
+print(c("Maximum" = maximum, "Minimum" = minimum))
+
+# b #
+#####
+
+len <- length(scores)
+
+total <- 0
+for (score in scores){
+  total <- total + score
+}
+
+avg <- total / len
+avg
+
+# c #
+#####
+
+# Removing the maximum number from the vector
+new_score <- scores[!scores %in% maximum]
+new_max <- new_score[1]
+
+for (score in new_score){
+  if (score > new_max){
+    new_max <- score
+  }
+}
+
+new_max
+
+# d #
+#####
+
+for (score in scores){
+  if (score > 100){
+    print("Value greater than 100 detected")
+  }
+}
+
+# e #
+#####
+
+new_score <- scores[!scores %in% minimum]
+new_min <- new_score[1]
+
+for (score in new_score){
+  if (score < new_min){
+    new_min <- score
+  }
+}
+
+final_score <- new_score[!new_score %in% new_min]
+len <- length(final_score)
+
+total <- 0
+for (score in final_score){
+  total <- total + score
+}
+
+avg <- total / len
+avg
